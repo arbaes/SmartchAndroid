4234357201225c969604755c45f50d0bcc711cee
s0me-1 <arnaud.baes@hotmail.com>
2017-12-19 18:22:59 +0100

    Init des différentes activités

---
+++ app/src/main/java/eu/creapix/louisss13/smartchandoid/MonitoredMatchesActivity.java
@@ -0,0 +1,13 @@
+package eu.creapix.louisss13.smartchandoid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MonitoredMatchesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_monitored_matches);
+    }
+}
---
+++ app/src/main/java/eu/creapix/louisss13/smartchandoid/PointCountActivity.java
@@ -0,0 +1,13 @@
+package eu.creapix.louisss13.smartchandoid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PointCountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_point_count);
+    }
+}
